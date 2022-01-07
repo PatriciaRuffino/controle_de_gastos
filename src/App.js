362beb40e06c7ffb29045f91e85379874716527f
PatriciaRuffino <sindelformr@gmail.com>
2022-01-07 10:56:14 -0300

    start do projeto cost, add de rotas, navbar e footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from "./components/layout/Navbar.js";
+import Home from "./components/pages/Home.js";
+import Contact from "./components/pages/Contact.js";
+import Company from "./components/pages/Company.js";
+import NewProject from "./components/pages/NewProject.js";
+import Projects from "./components/pages/Projects.js";
+import Container from "./components/layout/Container.js";
+
+import Footer from "./components/layout/Footer.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/newproject" element={<NewProject />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
   );
 }
 
