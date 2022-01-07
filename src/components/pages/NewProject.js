bca128231fb81df76f54dabe52ae34862e033e91
PatriciaRuffino <sindelformr@gmail.com>
2022-01-07 18:42:20 -0300

    adicionado useNavigate, mas com bug. Não redireciona a pagina para projetos

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,13 +1,37 @@
 import ProjectForm from '../project/ProjectForm';
 import styles from './NewProject.module.css';
+import {useNavigate} from 'react-router-dom';
 
 
 function NewProject () {
+
+    const navigate = useNavigate()
+
+    function creatPost(project){
+        //initialize cost and services
+        project.const =0
+        project.services = []
+
+        fetch('http://localhost:5001/projects', {
+            method: 'POST',
+            headers:{
+                'Content-type': 'aplication/json',
+            },
+            body: JSON.stringify(project)
+        })
+        .then((resp) => resp.json())
+        .then((data)=>{
+            console.log(data)
+        navigate('/projects',{message: 'Projeto criado com sucesso'} )
+        })
+        //redirect
+        .catch((err)=>console.log(err))
+    }
     return(
        <div className={styles.newproject_container}>
            <h1>Criar Projeto</h1>
            <p>Crie o seu projeto para depois adicionar os serviços</p>
-           <ProjectForm btnText="Criar projeto" />
+           <ProjectForm handleSubmit={creatPost} btnText="Criar projeto" />
        </div>
     )
 }
